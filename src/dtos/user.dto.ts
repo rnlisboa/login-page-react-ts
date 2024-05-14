52847d384d6d65ce7c728ae3d96a4741c65655b3
rnlisboa <dev.renanlisboa@gmail.com>
2024-05-14 14:37:37 -0300

    feat: cria dto para usuario

---
+++ src/dtos/user.dto.ts
@@ -0,0 +1,11 @@
+export interface UserDTO {
+    id: number;
+    name: string;
+    email: string;
+    is_active: boolean;
+    avatar: string | null;
+    type: string;
+    created: string;
+    modified: string;
+    role: string;
+}
