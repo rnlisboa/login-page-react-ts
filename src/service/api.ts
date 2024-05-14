009df79dc47a7d06b89d1013b8190c97b6768890
rnlisboa <dev.renanlisboa@gmail.com>
2024-05-14 14:38:53 -0300

    feat: configura api

---
+++ src/service/api.ts
@@ -0,0 +1,35 @@
+import axios from "axios"
+import { SERVICE } from "./auth"
+
+
+class Api {
+  public axios
+
+  constructor(serviceUrl: string = SERVICE.CORE) {
+    this.axios = axios.create({
+      baseURL: serviceUrl,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json;version=v1_web',
+      },
+    })
+    
+    this.axios.interceptors.response.use(
+      (response: any) => response,
+      async (error) => {
+        if (error.response.status === 401) {
+            //refreshtoken
+
+            return Promise.reject() 
+        }
+        else if ((error.response.status === 403)) {
+            //mandar pro login
+
+          return Promise.reject()
+        }
+      }
+    )
+  }
+}
+
+export { Api, SERVICE }
