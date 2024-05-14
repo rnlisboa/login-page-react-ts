e0c6d4761ba6d619d42a920ae23ddca9bc19cdcb
rnlisboa <dev.renanlisboa@gmail.com>
2024-05-14 11:41:34 -0300

    feat: atualiza importação das rotas da aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/routes.ts'
+import { router } from './routes/routes'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
