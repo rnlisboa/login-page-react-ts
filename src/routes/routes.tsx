7582cfe63b3def243d82c49dbe7f03b1823dc7dc
rnlisboa <dev.renanlisboa@gmail.com>
2024-05-15 18:00:44 -0300

    fix: corrige rotas

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,14 +2,21 @@ import { createBrowserRouter } from "react-router-dom";
 import PrivateRoute from "./privateRoute";
 import Login from "../pages/Login";
 import Profile from "../pages/profile";
+import App from "../App";
 
 export const router = createBrowserRouter([
     {
-        path: '/login',
-        element: <Login/>,
-    },
-    {
-        path: '/profile',
-        element: <PrivateRoute component={<Profile/> as any} authenticated/>,
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: '/login',
+                element: <Login />,
+            },
+            {
+                path: '/profile',
+                element: <PrivateRoute component={Profile} authenticated />,
+            }
+        ]
     }
 ])
