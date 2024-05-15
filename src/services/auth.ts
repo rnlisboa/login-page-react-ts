b4fa46f9d17d4e2cc6564c556e732281a1889a9e
rnlisboa <dev.renanlisboa@gmail.com>
2024-05-15 18:03:36 -0300

    feat: pega token do localstorage

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,6 +2,10 @@ interface serviceInt {
     [key: string]: string
 }
 
+export const getToken = () => {
+    return localStorage.getItem("token");
+}
+
 export const SERVICE: serviceInt = {
     CORE: 'https://api.homologation.cliqdrive.com.br/'
 }
