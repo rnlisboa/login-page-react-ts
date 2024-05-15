9a2cf3602e3f862e0cf61950621da9aa293e20a6
rnlisboa <dev.renanlisboa@gmail.com>
2024-05-15 18:04:26 -0300

    fix: corrige sintaxe

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { Outlet } from "react-router-dom"
 import { AuthProvider } from "./contexts/authContext/authContext";
 
-const queryClient = new QueryClient();
 
 function App() {
-
   return (
-    <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <Outlet />
       </AuthProvider>
-    </QueryClientProvider>
   )
 }
 
