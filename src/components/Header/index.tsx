cdc5b543040d8d674637c3f25f179916419827c3
rnlisboa <dev.renanlisboa@gmail.com>
2024-05-17 11:40:32 -0300

    feat: cria componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+interface HeaderProps {
+    children: ReactNode;
+    classes?: string[],
+}
+
+function Header({ children, classes }: HeaderProps) {
+    return (<header
+        className={`flex justify-end items-center px-6 bg-white w-[100%] h-[70px] border ${classes ? classes.join(' ') : ''}`}>
+        {children}
+    </header>)
+}
+
+export default Header;
