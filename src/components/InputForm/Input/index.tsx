c4e67a2f4f18466b7f0328173128eee539d25782
rnlisboa <dev.renanlisboa@gmail.com>
2024-05-19 17:39:50 -0300

    refactor: refatora componente input

--- src/components/InputForm/Input/index.tsx
+++ src/components/InputForm/Input/index.tsx
@@ -1,20 +1,15 @@
 import { InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<any> {
-
+    classes: string
 }
 
 
-function Input({...rest}:InputProps) {
+function Input({classes = '',...rest}:InputProps) {
     return (
         <input
-            type={rest.type}
-            id={rest.type}
-            className='border-none outline-none bg-input-bg text-input-text-color rounded-lg h-14 py-[18px] px-[20.25px] placeholder:text-input-text-color'
-            value={rest.value}
-            placeholder={rest.placeholder}
-            onChange={rest.onChange}
-            required
+            className={`border-none outline-none bg-input-bg text-input-text-color h-14 py-[18px] px-[20.25px] placeholder:text-input-text-color ${classes}`}
+            {...rest}
         />
     )
 }
